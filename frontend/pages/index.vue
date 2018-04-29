2c9b62061f694448036397d411a8b29d0118cee9
shahnirav <shah.nirav@me.com>
2018-04-29 23:24:59 +0100

    css

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,59 @@
 <template>
   <div>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="https://ayushway.com">
           <img src="/logo.png" alt="Ayushway Traditional Wellness">
         </a>
-
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
+      <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet"
+                   data-social-target="http://localhost:4000" target="_blank"
+                   href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=http://localhost:4000&amp;via=jgthms">
+              <span class="icon">
+                <i class="fa fa-twitter"></i>
+              </span>
+                  <span>
+                Tweet
+              </span>
+                </a>
+              </p>
+              <p class="control">
+                <a class="button is-primary"
+                   href="https://github.com/jgthms/bulma/releases/download/0.7.0/bulma-0.7.0.zip">
+              <span class="icon">
+                <i class="fa fa-download"></i>
+              </span>
+                  <span>Download</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
     <section class="hero is-large is-success">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Primary Bold Title
+          </h1>
+          <h2 class="subtitle">
+            Primary bold subtitle
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="hero is-large is-info">
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
@@ -29,7 +69,6 @@
 </template>
 
 <script>
-
   export default {}
 </script>
 
